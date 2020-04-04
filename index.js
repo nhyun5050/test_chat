141cbdd880b3c2a1f2f91d83b5f69d7b114d2849
huntmori04x22 <huntmori04x22@gmail.com>
2020-04-04 23:29:32 +0900

    express using static

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-var io = require("socket.io").listen("1004");
+/* Node.js 기본 내장 모듈 */
+const fs = require("fs")
 
-io.sockets.on('connection', function(socket){
-    socket.emit('news', {hello:'world'});
-    socket.on('my other event', function(data){
-        console.log(data);
-    });
+const express = require('express');
+
+const socket = require('socket.io');
+
+const http = require('http');
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socket(server);
+
+app.use('/css', express.static('./static/css'))
+app.use('/js', express.static('./static/js'))
+
+app.get("/", function(request, response){
+    //console.log("유저가 / 로 접속하였습니다.");
+    //response.send('hello express server');
+    fs.readFile('./static/index.html', function(err, data){
+        if(err){
+            response.send("에러");
+        }
+        else{
+            response.writeHead(200, {'Content-Type':'text/html'})
+            response.write(data)
+            response.end()
+        }
+    })
+}); 
+
+server.listen(8080, function(){
+    console.log('Server run....');
 });

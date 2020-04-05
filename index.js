381076cb20f759e0cd3df2d314ee624983b2bae6
huntmori04x22 <huntmori04x22@gmail.com>
2020-04-05 19:05:52 +0900

    default chat example

--- index.js
+++ index.js
@@ -34,12 +34,40 @@ app.get("/", function(request, response){
 io.sockets.on('connection', function(socket){
     console.log('user connected');
 
+    // 새로운 유저가 접속했을경우 다른 소켓에 알려줌
+    socket.on('newUser', function(name){
+        console.log(name+"님이 접속하였습니다.");
+        socket.name = name;
+        io.sockets.emit('update', { 
+                                    type:'connect', 
+                                    name:'SERVER', 
+                                    message:name+'님이 접속하였습니다.'
+        });
+    })
+
+    // 전송한 메시지 받기
+    socket.on('message', function(data){
+        // 받은 데이터에 누가 보냈는지 이름 추가
+        data.name = socket.name;
+        console.log(data);
+
+        // 보낸 사람을 제외한 나머지 유저에게 메시지 전송
+        socket.broadcast.emit('update', data);
+    })
+
     socket.on('send', function(data){
         console.log('sendded msg : '+data.msg);
     })
 
-    socket.on('disconnected', function(){
-        console.log('user disconnected')
+    socket.on('disconnect', function(){
+        console.log('user disconnected- name:'+socket.name)
+
+        //나가는 사람을 제오한 나머지 유저에게 퇴장 메시지 전송
+        socket.broadcast.emit('update', {
+            type:'disconnect'
+            ,name:'SERVER'
+            ,message:socket.name+ ' 님이 나가셨습니다.'
+        });
     })
 })
 
--- static/js/index.js
+++ static/js/index.js
@@ -3,6 +3,17 @@ var socket = io();
 socket.on('connect', function(){
     var input = document.getElementById('test');
     input.value = '접속 됨';
+
+    var name = prompt('반갑습니다!', '');
+    
+    name = !name? '익명' : name;
+    
+    socket.emit('newUser', name);
+})
+
+socket.on('update', function(data){
+    console.log(`${data.name}:${data.message}`);
+
 })
 
 function send()
@@ -11,5 +22,9 @@ function send()
 
     document.getElementById('test').value = '';
 
-    socket.emit('send',{msg:message});
+    //서버로 message 이벤트 전달
+    socket.emit('message', {
+        type:'message',
+        message:message
+    });
 }
